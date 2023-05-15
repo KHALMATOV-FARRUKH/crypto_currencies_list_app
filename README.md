5d43a49862ab1a63506e32c9c67a589c15bc2071
KHALMATOV-FARRUKH <khalmatov.farrukh@gmail.com>
2023-05-15 21:02:02 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# crypto_currencies_list_app
---
+++ android/app/src/main/kotlin/com/example/crypto_currencies_list_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.crypto_currencies_list_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
